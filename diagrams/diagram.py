2242a504f2662e19a1fb1a8f47f29f6320470002
johnatag <dev@johnatangao.com>
2024-11-05 14:05:21 -0500

    Change architecture to EC2s instead of Lambdas

--- diagrams/diagram.py
+++ diagrams/diagram.py
@@ -1,5 +1,6 @@
 from diagrams import Diagram, Cluster, Edge
-from diagrams.aws.compute import EC2, Lambda
+from diagrams.aws.compute import EC2
+from diagrams.onprem.container import Docker
 from diagrams.aws.iot import IotMqtt
 from diagrams.aws.network import VPC
 from diagrams.programming.language import Python
@@ -15,27 +16,28 @@ with Diagram("IoT-Driven Water Treatment Plant with PLC", show=False, direction=
         mqtt_broker = IotMqtt("Eclipse Mosquitto MQTT Broker")
         python_app = Python("Python PLC")
 
-    # Sensor Lambdas
-    with Cluster("Capteur de mouvement d'eau"):
-        flow_sensor = Lambda("Capteur de Flux de l'eau") # (m^3/s)
-        level_sensor = Lambda("Capteur du niveau d'eau") # (m)
+    with Cluster("EC2 Environnement Physique"):
+        # Sensor Dockers
+        with Cluster("Capteur de mouvement d'eau"):
+            flow_sensor = Docker("Capteur de Flux de l'eau") # (m^3/s)
+            level_sensor = Docker("Capteur du niveau d'eau") # (m)
 
-    with Cluster("Capteur de qualité d'eau"):
-        ph_sensor = Lambda("Capteur de pH de l'eau") # (pH)
-        turbidity_sensor = Lambda("Capteur de turbidité de l'eau") # (nephelometric turbidity units (NTU))
-        dissolved_oxygen_sensor = Lambda("Capteur d'oxygène dissout") # (mg/L)
-        conductivity_sensor = Lambda("Capteur de conductivité de l'eau") # siemens per meter (S/m)
+        with Cluster("Capteurs de qualité d'eau"):
+            ph_sensor = Docker("Capteur de pH de l'eau") # (pH)
+            turbidity_sensor = Docker("Capteur de turbidité de l'eau") # (nephelometric turbidity units (NTU))
+            dissolved_oxygen_sensor = Docker("Capteur d'oxygène dissout") # (mg/L)
+            conductivity_sensor = Docker("Capteur de conductivité de l'eau") # siemens per meter (S/m)
 
-    with Cluster("Capteur de température"):
-        temperature_sensor = Lambda("Capteur de température d'eau") # (C)
+        with Cluster("Capteur de température"):
+            temperature_sensor = Docker("Capteur de température d'eau") # (C)
 
-    with Cluster("Capteur de pression"):
-        pressure_sensor = Lambda("Capteur de pression de l'eau") # psi
+        with Cluster("Capteur de pression"):
+            pressure_sensor = Docker("Capteur de pression de l'eau") # psi
 
-    # Actuator Lambdas
-    with Cluster("Actuators"):
-        pump_actuator = Lambda("Pump Actuator\n(P101)")
-        valve_actuator = Lambda("Valve Actuator\n(MV101)")
+        # Actuator Dockers
+        with Cluster("Actuators"):
+            pump_actuator = Docker("Pump Actuator\n(P101)")
+            valve_actuator = Docker("Valve Actuator\n(MV101)")
 
     # Connections
     sensors = [flow_sensor, level_sensor, ph_sensor, turbidity_sensor, dissolved_oxygen_sensor, conductivity_sensor, pressure_sensor, temperature_sensor]
