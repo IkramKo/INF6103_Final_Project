996b40c643fb791e9c86903246c0de983cba27d8
IkramKo <kohil.ikram@hotmail.com>
2024-11-08 11:18:04 -0500

    Finished populating the db

--- deployment/sql/init.sql
+++ deployment/sql/init.sql
@@ -31,28 +31,39 @@ CREATE TABLE IF NOT EXISTS INF6103.Actuator (
 );
 
 ------------------- Populating IdealValue Table -------------------
--- C_ = Capteur
+-- T_ = Tank
+-- P_ = Pipe
 -- TRTM = Zone de traitement
-INSERT INTO INF6103.IdealValue(sensor_name, ideal_value, unit) 
-VALUES  ('C_Temperature_TRTM', 15, '°C'),
-        ('C_Level_TRTM', 450, 'L') -- can be measured with volume or height units; going for volume, same as for treated water
-        ('C_Conductivity_TRTM', 5000, 'S/m'), -- siemens per meter; value from https://atlas-scientific.com/blog/water-quality-parameters/
-        ('C_Dissolved_Oxygen_TRTM', 4, 'mg/L'),
-        ('C_Turbidity_TRTM', 9, 'NTU'), -- nephelometric turbidity units; value from https://atlas-scientific.com/blog/water-quality-parameters/
-        ('C_PH_TRTM', 5, 'pH'); -- we're assuming this waste water is acidic
-
 -- TRT = Zone traitée
+-- RTRTM = Boucle de retraitement
+-------------------------------------------------------------------
 INSERT INTO INF6103.IdealValue(sensor_name, ideal_value, unit) 
-VALUES  ('C_Temperature_TRT', 25, '°C'), -- should be between 20 and 40 degrees, so 25 should be fine
-        ('C_Level_TRT', 450, 'L')
-        ('C_Conductivity_TRT', 200, 'S/m'),
-        ('C_Dissolved_Oxygen_TRT', 7, 'mg/'),
-        ('C_Turbidity_TRT', 0.5, 'NTU'),
-        ('C_PH_TRT', 7.6, 'pH');
+VALUES  ('T_Temperature_TRTM', 15, '°C'),
+        ('T_Level_TRTM', 450, 'L'), -- can be measured with volume or height units; going for volume, same as for treated water
+        ('T_Conductivity_TRTM', 5000, 'S/m'), -- siemens per meter; value from https://atlas-scientific.com/blog/water-quality-parameters/
+        ('T_Dissolved_Oxygen_TRTM', 4, 'mg/L'),
+        ('T_Turbidity_TRTM', 9, 'NTU'), -- nephelometric turbidity units; value from https://atlas-scientific.com/blog/water-quality-parameters/
+        ('T_PH_TRTM', 5, 'pH'); -- we're assuming this waste water is acidic
+
+INSERT INTO INF6103.IdealValue(sensor_name, ideal_value, unit) 
+VALUES  ('T_Temperature_TRT', 25, '°C'), -- should be between 20 and 40 degrees, so 25 should be fine
+        ('T_Level_TRT', 450, 'L'),
+        ('T_Conductivity_TRT', 200, 'S/m'),
+        ('T_Dissolved_Oxygen_TRT', 7, 'mg/'),
+        ('T_Turbidity_TRT', 0.5, 'NTU'),
+        ('T_PH_TRT', 7.6, 'pH');
+
+-- All set to 0 because we're initializing both tanks to be nearly full, so all valves are closed, all pumps are off, and debit is null
+INSERT INTO INF6103.IdealValue(sensor_name, ideal_value, unit) 
+VALUES  ('P_Debit_TRT_Out', 0, 'L/s'),
+        ('P_Debit_TRTM_Out', 0, 'L/s'), -- Is also input of treated tank
+        ('P_Debit_RTRTM', 0, 'L/s'),
+        ('P_Debit_TRT_In', 0, 'L/s');
 
 ------------------- Populating Sensor Table -------------------
 -- Initially, this will essentially copy the IdealValue table entirely, but the current reading will eventually be modified during the simulation.
 -- This is done so that we will always have access to IdealValue in a separate table so the PLC can compare the current reading to the IdealValue and make commands accordingly.
+---------------------------------------------------------------
 INSERT INTO INF6103.Sensor (sensor_name, current_reading, unit)
 SELECT 
     id_val.sensor_name,
@@ -62,8 +73,18 @@ FROM
     INF6103.IdealValue id_val;  
 
 ------------------- Populating Actuators Table -------------------
+-- Valves
 INSERT INTO INF6103.Actuator(actuator_name, current_value, unit) 
-VALUES  ('Pompe_Entrée_Traitement', 18, 'L/s'),
-        ('Valve_Sortie_Traitée', 5, '%'); 
+VALUES  ('P_Valve_TRT_Out', 0, '%'), -- Opened at X%
+        ('P_Valve_TRTM_Out', 0, '%'),
+        ('P_Valve_RTRTM', 0, '%'),
+        ('P_Valve_TRT_In', 0, '%');
+
+-- Pumps
+INSERT INTO INF6103.Actuator(actuator_name, current_value, unit) 
+VALUES  ('P_Pump_TRT_Out', 0, 'L/s'), -- Also debit
+        ('P_Pump_TRTM_Out', 0, 'L/s'),
+        ('P_Pump_RTRTM', 0, 'L/s'),
+        ('P_Pump_TRT_In', 0, 'L/s');
 
 
